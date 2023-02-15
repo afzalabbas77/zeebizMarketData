b9eb74acd0895795541aefe8445e5cac262293cd
Abzal Abbas <77445243+afzalabbas77@users.noreply.github.com>
2023-02-15 11:15:01 +0530

    Update BreadcrumTopadd.js

--- src/BreadcrumTopadd.js
+++ src/BreadcrumTopadd.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import bannerimg from './images/bannr970X90.gif';
 const BreadcrumTopadd = () => {
+    const  [btnState, setBtnState] = useState(false)
+  function handleClick(){
+    setBtnState(btnState => ! btnState)
+  }
+  let toggleClassCheck = btnState ? ' heightauto' : '';
+    
     return (
         <>
 
@@ -9,7 +15,7 @@ const BreadcrumTopadd = () => {
             <div className="breadcrum"><ul><li><NavLink to = "/">Home</NavLink></li><li><NavLink to = "/">Markets</NavLink></li></ul></div>
           <div className="topbanner mgbt10"><img src={bannerimg} alt="" className="img-res" /></div>
 
-          <div className="biztagsbox">
+          <div className="biztagsbox" className= {`biztagsbox${toggleClassCheck}`}>
             <ul className="biztags">
               <li><NavLink to = "/">Top gainers</NavLink></li>
               <li><NavLink to = "/">Top Loosers</NavLink></li>
@@ -25,7 +31,7 @@ const BreadcrumTopadd = () => {
               <li><NavLink to = "/">Historical returns</NavLink></li>
               
             </ul>
-            <div className="dot-elastic"></div>
+            <div className="dot-elastic" onClick={handleClick}></div>
           </div>
 
         </div>
